ba7e0f4e5cc9bb78f789803420702765b9600e57
Xihan Zhon <xihanzhon@Xihans-Laptop.local>
2025-10-12 20:50:51 -0400

    Initial commit - frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
