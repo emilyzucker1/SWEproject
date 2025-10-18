2de40bad1e4cee9d3645f755220273201da660ad
Xihan Zhon <xihanzhon@Xihans-Laptop.local>
2025-10-17 23:31:25 -0400

    Added Home (Welcome) Page and updated routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
