0ad89d36d8e0b3a5d16e32223f36fe99469ac625
devjgator <lijj1743@gmail.com>
2025-10-21 17:28:33 -0400

    Database schema defined and test script

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,9 +1,19 @@
+import mongoose from "mongoose";
+
+const gifSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  dateAdded: { type: Date, default: Date.now },
+  likes: { type: Number, default: 0 }
+});
+
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-  });
-  
-  const User = mongoose.model("User", userSchema);
-  
-  export default User;
-  
+  id: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  gifs: [gifSchema],
+  following: [
+    { type: mongoose.Schema.Types.String, ref: "User" }
+  ]
+});
+
+export const User = mongoose.model("User", userSchema);  
