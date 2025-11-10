9b8aee9a41242d6620a3baf5725cbb8fb88b058a
Xihan Zhou <121860511+lililuvicky0204@users.noreply.github.com>
2025-11-10 17:09:00 -0500

    Update App.jsx
    Add new page mainpage part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
 import Main from "./pages/Main";
+import MainPage from "./pages/mainpage";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/mainpage" element={<MainPage />} />
       </Routes>
     </Router>
   );
